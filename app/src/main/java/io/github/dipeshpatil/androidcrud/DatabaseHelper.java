038bcadd0965fb5d1db4b46ae8a4556c7ebf4759
Dipesh Patil <thedipeshpatil@gmail.com>
2021-03-13 03:53:39 +0530

    Added SQLite Support for Offline Capabilities

--- app/src/main/java/io/github/dipeshpatil/androidcrud/DatabaseHelper.java
+++ app/src/main/java/io/github/dipeshpatil/androidcrud/DatabaseHelper.java
@@ -1,5 +1,152 @@
 package io.github.dipeshpatil.androidcrud;
 
-public class DatabaseHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DB_NAME = "Movies.db";
+    public static final String TABLE_NAME = "movies";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "title";
+    public static final String COL_3 = "plot";
+    public static final String COL_4 = "rating";
+    public static final String COL_5 = "poster";
+    public static final String COL_6 = "genre";
+    public static final String COL_7 = "year";
+    public static final String COL_8 = "released";
+    public static final String COL_9 = "actors";
+    public static final String COL_10 = "director";
+    public static final String COL_11 = "title_slug";
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_NAME + "(" +
+                COL_1 + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COL_2 + " TEXT, " +
+                COL_3 + " TEXT, " +
+                COL_4 + " TEXT, " +
+                COL_5 + " TEXT, " +
+                COL_6 + " TEXT, " +
+                COL_7 + " TEXT, " +
+                COL_8 + " TEXT, " +
+                COL_9 + " TEXT, " +
+                COL_10 + " TEXT, " +
+                COL_11 + " TEXT " +
+                ")"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean insertData(
+            String title,
+            String plot,
+            String rating,
+            String poster,
+            String genre,
+            String year,
+            String released,
+            String actors,
+            String directors,
+            String title_slug
+    ) {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL_2, title);
+        contentValues.put(COL_3, plot);
+        contentValues.put(COL_4, rating);
+        contentValues.put(COL_5, poster);
+        contentValues.put(COL_6, genre);
+        contentValues.put(COL_7, year);
+        contentValues.put(COL_8, released);
+        contentValues.put(COL_9, actors);
+        contentValues.put(COL_10, directors);
+        contentValues.put(COL_11, title_slug);
+
+        long result = sqLiteDatabase.insert(
+                TABLE_NAME,
+                null,
+                contentValues
+        );
+
+        if (result == -1) {
+            return false;
+        }
+        return true;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        return sqLiteDatabase.rawQuery(
+                "SELECT * FROM " + TABLE_NAME,
+                null
+        );
+    }
+
+    public boolean alreadyExists(String title_slug) {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        return sqLiteDatabase.rawQuery(
+                "SELECT title_slug FROM " + TABLE_NAME + " WHERE title_slug = ?",
+                new String[]{title_slug}
+        ).getCount() != 0;
+    }
+
+    public boolean updateData(
+            String id,
+            String title,
+            String plot,
+            String rating,
+            String poster,
+            String genre,
+            String year,
+            String released,
+            String actors,
+            String directors,
+            String title_slug
+    ) {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL_2, title);
+        contentValues.put(COL_3, plot);
+        contentValues.put(COL_4, rating);
+        contentValues.put(COL_5, poster);
+        contentValues.put(COL_6, genre);
+        contentValues.put(COL_7, year);
+        contentValues.put(COL_8, released);
+        contentValues.put(COL_9, actors);
+        contentValues.put(COL_10, directors);
+        contentValues.put(COL_11, title_slug);
+
+        sqLiteDatabase.update(
+                TABLE_NAME,
+                contentValues,
+                "ID = ?",
+                new String[]{id}
+        );
+
+        return true;
+    }
+
+    public boolean deleteData(String id) {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        sqLiteDatabase.delete(
+                TABLE_NAME,
+                "ID = ?",
+                new String[]{id}
+        );
+        return true;
+    }
 }
--- app/src/main/java/io/github/dipeshpatil/androidcrud/MainActivity.java
+++ app/src/main/java/io/github/dipeshpatil/androidcrud/MainActivity.java
@@ -16,6 +16,7 @@ import com.android.volley.toolbox.Volley;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.imangazaliev.slugify.Slugify;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 import com.theophrast.ui.widget.SquareImageView;
@@ -29,6 +30,7 @@ import io.github.dipeshpatil.androidcrud.Movies.Movie;
 public class MainActivity extends AppCompatActivity {
     private static final String API_KEY = "e2716675";
     private FirebaseFirestore db;
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +38,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         db = FirebaseFirestore.getInstance();
+        databaseHelper = new DatabaseHelper(this);
 
         EditText movieTitle = findViewById(R.id.movie_title);
 
         Button fetchButton = findViewById(R.id.fetch_button);
 
         TextView movieName = findViewById(R.id.movie_name);
+        TextView moviePlot = findViewById(R.id.movie_plot);
         SquareImageView moviePoster = findViewById(R.id.movie_poster);
 
         ProgressDialog dataDialog = new ProgressDialog(this);
-        dataDialog.setMessage("Loading");
+        dataDialog.setMessage("Fetching");
         dataDialog.setCanceledOnTouchOutside(false);
 
         String URL = "https://www.omdbapi.com/?apikey=" + API_KEY + "&t=";
@@ -54,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
             dataDialog.show();
             String title = movieTitle.getText().toString();
             String requestURL = URL + URLEncoder.encode(title);
-            Toast.makeText(this, requestURL, Toast.LENGTH_SHORT).show();
             StringRequest request = new StringRequest(requestURL,
                     response -> {
                         Log.d("VOLLEY_RESPONSE", response);
@@ -62,25 +65,68 @@ public class MainActivity extends AppCompatActivity {
                         Gson gson = builder.create();
                         Movie movie = gson.fromJson(response, Movie.class);
 
-                        String movieNameText = movie.getTitle();
+                        String movieTitleText = movie.getTitle();
+                        String moviePlotText = movie.getPlot();
+                        String movieRatingText = movie.getImdbRating();
                         String moviePosterURL = movie.getPoster();
-                        String movieRating = movie.getImdbRating();
+                        String movieGenreText = movie.getGenre();
+                        String movieYearText = movie.getYear();
+                        String movieReleased = movie.getReleased();
+                        String movieActors = movie.getActors();
+                        String movieDirector = movie.getDirector();
+
+                        Slugify slugify = new Slugify();
+                        String title_slug = slugify.slugify(movieTitleText.toLowerCase());
 
                         Map<String, Object> movieMap = new HashMap<>();
-                        movieMap.put("title", movieNameText);
+                        movieMap.put("title_slug", title_slug);
+                        movieMap.put("title", movieTitleText);
+                        movieMap.put("plot", moviePlotText);
+                        movieMap.put("rating", movieRatingText);
                         movieMap.put("poster", moviePosterURL);
-                        movieMap.put("rating", movieRating);
+                        movieMap.put("genre", movieGenreText);
+                        movieMap.put("year", movieYearText);
+                        movieMap.put("released", movieReleased);
+                        movieMap.put("actors", movieActors);
+                        movieMap.put("directors", movieDirector);
 
-                        movieName.setText(movieNameText);
+                        movieName.setText(movieTitleText);
+                        moviePlot.setText(moviePlotText);
+
                         Picasso.get().load(moviePosterURL).into(moviePoster, new Callback() {
                             @Override
                             public void onSuccess() {
                                 dataDialog.dismiss();
+
                                 db.collection("movies")
-                                        .add(movieMap)
+                                        .document(title_slug)
+                                        .set(movieMap)
                                         .addOnSuccessListener(
-                                                documentReference -> Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_SHORT).show())
-                                        .addOnFailureListener(e -> Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show());
+                                                documentReference -> {
+                                                    if (!databaseHelper.alreadyExists(title_slug)) {
+                                                        boolean success = databaseHelper.insertData(
+                                                                movieTitleText,
+                                                                moviePlotText,
+                                                                movieRatingText,
+                                                                moviePosterURL,
+                                                                movieGenreText,
+                                                                movieYearText,
+                                                                movieReleased,
+                                                                movieActors,
+                                                                movieDirector,
+                                                                title_slug
+                                                        );
+                                                        if (success)
+                                                            Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                                                        else
+                                                            Toast.makeText(MainActivity.this, "SQLite Failed!", Toast.LENGTH_LONG).show();
+                                                    } else
+                                                        Toast.makeText(MainActivity.this, "Already Exists!", Toast.LENGTH_SHORT).show();
+                                                }
+                                        )
+                                        .addOnFailureListener(
+                                                e -> Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show()
+                                        );
                             }
 
                             @Override
